8f160cddeadb6dede1d985aca63cd0158663dba4
Ali Rastgoo <ali.rastgoo1360@gmail.com>
2025-09-03 11:56:05 -0300

    ReAct in details

--- react-search-agent/main.py
+++ react-search-agent/main.py
@@ -14,20 +14,22 @@ load_dotenv()
 
 tool = [TavilySearch()]
 llm = ChatOpenAI(temperature=0, model="gpt-4")
+
 react_prompt = hub.pull("hwchase17/react")
+
 output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
 react_prompt_with_format_instructions = PromptTemplate(
-    template= REACT_PROMPT_WITH_INSTRUCTIONS,
-    input_variables=["input", "agent_scratchpad", "tool_names"]
+    template=REACT_PROMPT_WITH_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
 ).partial(format_instructions=output_parser.get_format_instructions())
 
 agent = create_react_agent(
-    llm= llm, 
-    tools= tool, 
-    prompt= react_prompt_with_format_instructions)
+    llm=llm, tools=tool, prompt=react_prompt_with_format_instructions
+)
 
 agent_executor = AgentExecutor(agent=agent, tools=tool, verbose=True)
-extract_output = RunnableLambda(lambda x : x["output"])
+extract_output = RunnableLambda(lambda x: x["output"])
 parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 
 chain = agent_executor | extract_output | parse_output
